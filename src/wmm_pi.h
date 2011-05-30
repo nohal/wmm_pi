acd69d5b7ae87729134b48312752bc28dec52cf4
Pavel Kalian <pavel@kalian.cz>
2011-05-30 18:19:16 +0200

    API version bump to 1.5

--- src/wmm_pi.h
+++ src/wmm_pi.h
@@ -41,7 +41,7 @@
 #define     PLUGIN_VERSION_MINOR    3
 
 #define     MY_API_VERSION_MAJOR    1
-#define     MY_API_VERSION_MINOR    4
+#define     MY_API_VERSION_MINOR    5
 
 #include "../../../include/ocpn_plugin.h"
 
