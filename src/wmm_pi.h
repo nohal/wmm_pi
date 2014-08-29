b8eb75799375f777da2e2a482e3f6e5b32d9bb0a
Pavel Kalian <pavel@kalian.cz>
2014-08-29 12:53:53 -0500

    Fix tabs, indentation and line endings.

--- src/wmm_pi.h
+++ src/wmm_pi.h
@@ -8,21 +8,21 @@
  ***************************************************************************
  *   Copyright (C) 2011 by Pavel Kalian   *
  *   $EMAIL$   *
- *                                                                         *
+ *                                                 *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
+ *   (at your option) any later version.                         *
+ *                                                 *
+ *   This program is distributed in the hope that it will be useful,     *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of      *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       *
+ *   GNU General Public License for more details.                  *
+ *                                                 *
  *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.             *
+ *   along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                           *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,  USA.         *
  ***************************************************************************
  */
 
@@ -57,17 +57,17 @@
 //    The PlugIn Class Definition
 //----------------------------------------------------------------------------------------------------------
 
-#define WMM_TOOL_POSITION    -1          // Request default positioning of toolbar tool
+#define WMM_TOOL_POSITION    -1        // Request default positioning of toolbar tool
 class wmm_pi;
 
 class WmmUIDialog : public WmmUIDialogBase
-{		
+{      
 public:
     WmmUIDialog( wmm_pi &_wmm_pi, wxWindow* parent, wxWindowID id = wxID_ANY, const wxString& title = _T("WMM"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxSize( 250,495 ), long style = wxCAPTION|wxDEFAULT_DIALOG_STYLE|wxTAB_TRAVERSAL ) : WmmUIDialogBase( parent, id, title, pos, size, style ), m_wmm_pi(_wmm_pi) {}
 
     void EnablePlotChanged( wxCommandEvent& event );
     void PlotSettings( wxCommandEvent& event );
-	
+    
 protected:
     wmm_pi &m_wmm_pi;
 };
@@ -85,100 +85,100 @@ WmmPlotSettingsDialog( wxWindow* parent, wxWindowID id = wxID_ANY, const wxStrin
 class wmm_pi : public opencpn_plugin_18
 {
 public:
-      wmm_pi(void *ppimgr);
+    wmm_pi(void *ppimgr);
 
 //    The required PlugIn Methods
-      int Init(void);
-      bool DeInit(void);
+    int Init(void);
+    bool DeInit(void);
 
-      int GetAPIVersionMajor();
-      int GetAPIVersionMinor();
-      int GetPlugInVersionMajor();
-      int GetPlugInVersionMinor();
-      wxBitmap *GetPlugInBitmap();
-      wxString GetCommonName();
-      wxString GetShortDescription();
-      wxString GetLongDescription();
+    int GetAPIVersionMajor();
+    int GetAPIVersionMinor();
+    int GetPlugInVersionMajor();
+    int GetPlugInVersionMinor();
+    wxBitmap *GetPlugInBitmap();
+    wxString GetCommonName();
+    wxString GetShortDescription();
+    wxString GetLongDescription();
 
 //    The required override PlugIn Methods
-      void SetCursorLatLon(double lat, double lon);
-      void SetPositionFix(PlugIn_Position_Fix &pfix);
+    void SetCursorLatLon(double lat, double lon);
+    void SetPositionFix(PlugIn_Position_Fix &pfix);
 
-      void RenderOverlayBoth(wxDC *dc, PlugIn_ViewPort *vp);
-      bool RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp);
-      bool RenderGLOverlay(wxGLContext *pcontext, PlugIn_ViewPort *vp);
-      void RecomputePlot();
+    void RenderOverlayBoth(wxDC *dc, PlugIn_ViewPort *vp);
+    bool RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp);
+    bool RenderGLOverlay(wxGLContext *pcontext, PlugIn_ViewPort *vp);
+    void RecomputePlot();
 
-      int GetToolbarToolCount(void);
-      void ShowPreferencesDialog( wxWindow* parent );
-      void ShowPlotSettingsDialog( wxCommandEvent& event );
+    int GetToolbarToolCount(void);
+    void ShowPreferencesDialog( wxWindow* parent );
+    void ShowPlotSettingsDialog( wxCommandEvent& event );
 
-      void OnToolbarToolCallback(int id);
+    void OnToolbarToolCallback(int id);
 
 //    Optional plugin overrides
-      void SetColorScheme(PI_ColorScheme cs);
-      void SetPluginMessage(wxString &message_id, wxString &message_body);
+    void SetColorScheme(PI_ColorScheme cs);
+    void SetPluginMessage(wxString &message_id, wxString &message_body);
 
-      void SetShowPlot(bool showplot) { m_bShowPlot = showplot; }
+    void SetShowPlot(bool showplot) { m_bShowPlot = showplot; }
 
 //    Other public methods
-      void SetWmmDialogX    (int x){ m_wmm_dialog_x = x;};
-      void SetWmmDialogY    (int x){ m_wmm_dialog_y = x;}
+    void SetWmmDialogX    (int x){ m_wmm_dialog_x = x;};
+    void SetWmmDialogY    (int x){ m_wmm_dialog_y = x;}
 
-      void OnWmmDialogClose();
-      void ShowPlotSettings();
+    void OnWmmDialogClose();
+    void ShowPlotSettings();
 
 //    WMM Declarations
-      WMMtype_MagneticModel *MagneticModel, *TimedMagneticModel;
-      WMMtype_Ellipsoid Ellip;
-      WMMtype_CoordSpherical CoordSpherical;
-	WMMtype_CoordGeodetic CoordGeodetic;
-	WMMtype_Date UserDate;
-	WMMtype_GeoMagneticElements GeoMagneticElements;
-	WMMtype_Geoid Geoid;
-	wxString filename;
+    WMMtype_MagneticModel *MagneticModel, *TimedMagneticModel;
+    WMMtype_Ellipsoid Ellip;
+    WMMtype_CoordSpherical CoordSpherical;
+    WMMtype_CoordGeodetic CoordGeodetic;
+    WMMtype_Date UserDate;
+    WMMtype_GeoMagneticElements GeoMagneticElements;
+    WMMtype_Geoid Geoid;
+    wxString filename;
 
-      wxWindow         *m_parent_window;
+    wxWindow       *m_parent_window;
 
 private:
-      wxFileConfig     *m_pconfig;
-      bool              LoadConfig(void);
-      bool              SaveConfig(void);
+    wxFileConfig     *m_pconfig;
+    bool          LoadConfig(void);
+    bool          SaveConfig(void);
 
-      WmmUIDialog      *m_pWmmDialog;
+    WmmUIDialog    *m_pWmmDialog;
 
-      int               m_wmm_dialog_x, m_wmm_dialog_y;
-      int               m_display_width, m_display_height;
-      int               m_iViewType;
-      bool              m_bShowPlotOptions;
-      bool              m_bShowAtCursor;
-      bool              m_bShowLiveIcon;
-      int               m_iOpacity;
+    int           m_wmm_dialog_x, m_wmm_dialog_y;
+    int           m_display_width, m_display_height;
+    int           m_iViewType;
+    bool          m_bShowPlotOptions;
+    bool          m_bShowAtCursor;
+    bool          m_bShowLiveIcon;
+    int           m_iOpacity;
 
-      wxString          m_LastVal;
+    wxString        m_LastVal;
 
-      int               m_leftclick_tool_id;
+    int           m_leftclick_tool_id;
 
-      wxString          AngleToText(double angle);
+    wxString        AngleToText(double angle);
 
-      bool              m_bCachedPlotOk, m_bShowPlot;
-      MagneticPlotMap   m_DeclinationMap, m_InclinationMap, m_FieldStrengthMap;
-      wxDateTime        m_MapDate;
-      int               m_MapStep;
-      int               m_MapPoleAccuracy;
+    bool          m_bCachedPlotOk, m_bShowPlot;
+    MagneticPlotMap   m_DeclinationMap, m_InclinationMap, m_FieldStrengthMap;
+    wxDateTime      m_MapDate;
+    int           m_MapStep;
+    int           m_MapPoleAccuracy;
 
-      void              RearrangeWindow();
-      wxString          m_wmm_dir;
-      bool              m_buseable, m_busegeoid;
+    void          RearrangeWindow();
+    wxString        m_wmm_dir;
+    bool          m_buseable, m_busegeoid;
 
-      void              SendVariationAt(double lat, double lon, int year, int month, int day);
-      void              SendBoatVariation();
-      void              SendCursorVariation();
+    void          SendVariationAt(double lat, double lon, int year, int month, int day);
+    void          SendBoatVariation();
+    void          SendCursorVariation();
 
-      WMMtype_GeoMagneticElements m_cursorVariation;
-      WMMtype_GeoMagneticElements m_boatVariation;
+    WMMtype_GeoMagneticElements m_cursorVariation;
+    WMMtype_GeoMagneticElements m_boatVariation;
 
-      bool m_bComputingPlot;
+    bool m_bComputingPlot;
 };
 
 int WMM_setupMagneticModel(char *data, WMMtype_MagneticModel * MagneticModel);
